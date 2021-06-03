74dd37c86d6260a241f26f42d8da30e2a319ca96
adrifcastr <adrifcastr@gmail.com>
2021-06-03 15:36:47 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import Collection from '@discordjs/collection';
+import type { PokemonSet } from '@pkmn/sets';
+
+export async function summaryScreen(data: PokemonSet, { animated }: { animated?: boolean }): Promise<any> {
+
+}
+
+export async function partyScreen(data: PokemonSet[] | Collection<String, PokemonSet>, { animated }: { animated?: boolean }): Promise<any> {
+
+}
